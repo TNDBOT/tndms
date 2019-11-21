16aeac0fbe32f4e610da98cff99331f1cb54983d
TNDBOT <58036259+TNDBOT@users.noreply.github.com>
2019-11-21 17:18:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ const gif = require("gif-search");
 const client = new Discord.Client({disableEveryone: true});
 
 var prefix = "$";
-var adminprefix = '$'
+var adminprefix = '$-'
 /////////////////////////
 ////////////////////////
 
